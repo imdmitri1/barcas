768054aecbbfcb4b9ab90c62f5e1d5839d17bb61
Dmitri Stefanisin <email.ds@protonmail.com>
2018-03-15 22:56:12 -0500

    add part of form for new boat and raw controller method

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -8,4 +8,28 @@ class BoatsController < ApplicationController
     @boat = Boat.find_by(id: params[:id])
   end
 
+  def new
+    # @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(name: params[:boat][:name],
+                     description: params[:boat][:description],
+                     brand_id: rand(0..6),
+                     type_id: rand(0..6)) #!!!!!!????
+    if @boat.save
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
